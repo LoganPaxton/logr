b6441ec0926d8e3bbb407b74bf7f3da1f1365a98
Logan Paxton <111173907+LoganPaxton@users.noreply.github.com>
2024-07-12 08:25:53 -0400

    Create types

--- logr.js
+++ logr.js
@@ -23,30 +23,68 @@ function newLog() {
   });
 }
 
-function add(text = "") {
+function add(text="", type="") {
+  if (type.length == 0) {
+    console.warn("Please specify a type.")
+  }
   if (text.length === 0) {
     console.warn("Text length cannot be 0.");
     return;
   }
+  if (type.toLowerCase() == "log") {
+    const now = new Date();
+    const hours = String(now.getHours()).padStart(2, "0");
+    const minutes = String(now.getMinutes()).padStart(2, "0");
+    const seconds = String(now.getSeconds()).padStart(2, "0");
 
-  const now = new Date();
-  const hours = String(now.getHours()).padStart(2, "0");
-  const minutes = String(now.getMinutes()).padStart(2, "0");
-  const seconds = String(now.getSeconds()).padStart(2, "0");
+    const toAppend = `[LOG] ${hours}-${minutes}-${seconds}: ${text}\n`;
+    const file_path = getCurrentDateTime();
 
-  const toAppend = `[LOG] ${hours}-${minutes}-${seconds}: ${text}\n`;
-  const file_path = getCurrentDateTime();
+    fs.appendFile(file_path, toAppend, function (err) {
+      if (err) {
+        console.error(`Error appending to log file: ${err.message}`);
+      } else {
+        //console.log(`Added log entry to file: ${file_path}`);
+      }
+    });
+  } else if (type.toLowerCase == "error") {
+    const now = new Date();
+    const hours = String(now.getHours()).padStart(2, "0");
+    const minutes = String(now.getMinutes()).padStart(2, "0");
+    const seconds = String(now.getSeconds()).padStart(2, "0");
 
-  fs.appendFile(file_path, toAppend, function (err) {
-    if (err) {
-      console.error(`Error appending to log file: ${err.message}`);
-    } else {
-      //console.log(`Added log entry to file: ${file_path}`);
-    }
-  });
+    const toAppend = `[ERROR] ${hours}-${minutes}-${seconds}: ${text}\n`;
+    const file_path = getCurrentDateTime();
+
+    fs.appendFile(file_path, toAppend, function (err) {
+      if (err) {
+        console.error(`Error appending to log file: ${err.message}`);
+      } else {
+        //console.log(`Added log entry to file: ${file_path}`);
+      }
+    });
+  } else if (type.toLowerCase() == "warn") {
+    const now = new Date();
+    const hours = String(now.getHours()).padStart(2, "0");
+    const minutes = String(now.getMinutes()).padStart(2, "0");
+    const seconds = String(now.getSeconds()).padStart(2, "0");
+
+    const toAppend = `[WARNING] ${hours}-${minutes}-${seconds}: ${text}\n`;
+    const file_path = getCurrentDateTime();
+
+    fs.appendFile(file_path, toAppend, function (err) {
+      if (err) {
+        console.error(`Error appending to log file: ${err.message}`);
+      } else {
+        //console.log(`Added log entry to file: ${file_path}`);
+      }
+    });
+  }
 }
 
+// Deprecated Since V1.1
 function error(text="") {
+  console.warn("The error function is now deprecated. Consider switching to add.")
   if (text.length === 0) {
     console.warn("Text length cannot be 0.");
     return;
